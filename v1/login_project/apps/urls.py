538aadc064a178f24a2db100215c7c415ceeb8d3
shreya <shreya.v@techpath.biz>
2024-03-05 14:39:35 +0530

    add employees data

--- v1/login_project/apps/urls.py
+++ v1/login_project/apps/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.login_view, name='login'),
+    path('', views.Index, name="home"),
+    path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
+    path('add/',views.Add_Employee , name='add-employee'),
+    
 ]
--- v1/login_project/apps/views.py
+++ v1/login_project/apps/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from apps.models import Employees
 
 # Create your views here.
 
@@ -7,3 +8,48 @@ def login_view(request):
 
 def signup_view(request):
     return render(request, 'main/signup.html')
+
+def Index(request):
+    return render(request, 'web/index.html')
+
+
+def Index(request):
+    emp = Employees.objects.all()
+    context = {
+        "emp": emp
+    }
+    return render(request, 'web/index.html',context)
+
+def Add_Employee(request):
+    if  request.method == 'POST':
+        name =  request.POST.get('emp_name')
+        email = request.POST.get('emp_email') 
+        phone = request.POST.get('emp_phone')    
+        address = request.POST.get('emp_address')
+    
+        emp = Employees(
+            name= name,
+            email= email,
+            phone = phone,
+            address= address   
+        )
+        emp.save()
+        redirect('home')
+    return render(request, 'web/index.html')
+
+def edit_Employee(request):
+    if  request.method == 'POST':
+        name =  request.POST.get('emp_name')
+        email = request.POST.get('emp_email') 
+        phone = request.POST.get('emp_phone')    
+        address = request.POST.get('emp_address')
+    
+        emp = Employees(
+            name= name,
+            email= email,
+            phone = phone,
+            address= address   
+        )
+        emp.save()
+        redirect('home')
+    return render(request, 'web/index.html')
